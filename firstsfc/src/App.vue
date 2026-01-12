a1e187a5d036d55296105aa48c95c2fb0db34d8a
Mark Harvey Dacara <medacara@student.apc.edu.ph>
2026-01-12 15:27:10 +0800

    another message

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -6,7 +6,7 @@
 export default {
   data() {
     return {
-      message: 'Hello World. This is Mark Harvey, signing out. I lab u all'
+      message: 'Hello World. This is Mark Harvey, signing out. I lab u all. Ang galing galing'
     };
   }
 };
