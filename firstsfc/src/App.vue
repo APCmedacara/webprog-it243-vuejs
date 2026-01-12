e5bbd931264279a8213ca1eb1cbaf412b8670427
Mark Harvey Dacara <medacara@student.apc.edu.ph>
2026-01-12 06:29:32 +0000

    created initial vue application

---
+++ firstsfc/src/App.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <h1>You did it Harvey Dacara galingg!</h1>
+  <h3>Isa kang tunay na henyo</h3>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
