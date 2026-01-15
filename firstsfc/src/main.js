e2c7948877c74a5e6bfd8ed36780357057942d54
Mark Harvey Dacara <medacara@student.apc.edu.ph>
2026-01-15 14:52:32 +0800

    try

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -6,6 +6,6 @@ import FoodItem from './components/FoodItem2.vue'
 
 const app = createApp(App)
 app.component('food-item', FoodItem)
-app.component('food-item2', FoodItem2)
+app.component('food-item2', FoodItem)
 
 app.mount('#app')
